64a7e6fe5204ef5a5c0792b6fca9550c4d18c22b
jenshadlich <jenshadlich@googlemail.com>
2015-04-28 22:04:42 +0200

    scales an image

---
+++ src/main/java/de/jeha/photo/mosaic/ImageScaler.java
@@ -0,0 +1,20 @@
+package de.jeha.photo.mosaic;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+class ImageScaler {
+
+    public static BufferedImage scale(BufferedImage input, int width, int height) {
+        final Image scaled = input.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        final BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        final Graphics g = output.getGraphics();
+        g.drawImage(scaled, 0, 0, null);
+        g.dispose();
+
+        return output;
+    }
+}
