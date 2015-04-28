b67feda1fa7253d675cfcefac317341fdf792625
jenshadlich <jenshadlich@googlemail.com>
2015-04-28 23:23:23 +0200

    add todo

--- src/main/java/de/jeha/photo/mosaic/PhotoMosaic.java
+++ src/main/java/de/jeha/photo/mosaic/PhotoMosaic.java
@@ -13,6 +13,11 @@ public class PhotoMosaic {
     private static final int TILE_WIDTH = 40;
     private static final int TILE_HEIGHT = 30;
 
+    // TODO: collect cmd line arguments with args4j
+    private String targetFile;
+    private String inputDirectory;
+    private boolean recursive = false;
+
     public static void main(String... args) throws IOException {
         System.setProperty("java.awt.headless", "true");
 
