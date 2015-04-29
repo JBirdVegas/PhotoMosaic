20ae4881e946b8ebd8407499d96d56713a837716
jenshadlich <jenshadlich@googlemail.com>
2015-04-29 10:46:21 +0200

    add logback config

--- src/main/java/de/jeha/photo/mosaic/PhotoMosaic.java
+++ src/main/java/de/jeha/photo/mosaic/PhotoMosaic.java
@@ -1,5 +1,8 @@
 package de.jeha.photo.mosaic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,6 +12,8 @@ import java.io.IOException;
  * @author jenshadlich@googlemail.com
  */
 public class PhotoMosaic {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PhotoMosaic.class);
 
     private static final int TILE_WIDTH = 40;
     private static final int TILE_HEIGHT = 30;
@@ -21,10 +26,11 @@ public class PhotoMosaic {
     public static void main(String... args) throws IOException {
         System.setProperty("java.awt.headless", "true");
 
-        BufferedImage image = ImageIO.read(new File("src/test/resources/sprd_logo_small.jpg"));
+        BufferedImage image = ImageIO.read(new File("src/test/resources/IMG_20150320_142153.jpg"));
+        LOG.info("h={}, w={}", image.getHeight(), image.getWidth());
 
         BufferedImage scaledImage = ImageScaler.scale(image, TILE_WIDTH, TILE_HEIGHT);
-        ImageIO.write(scaledImage, "png", new File("target/sprd_logo_small_scaled.png"));
+
     }
 
 }
